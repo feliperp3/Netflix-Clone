d7e4f2076dd8ce726678c9d213db57fdaac50f8f
feliperp3 <feliperibeiropeixoto@gmail.com>
2023-01-16 22:40:56 -0300

    initial commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "f02dead1bb868d471ae217bf440dcca6"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
